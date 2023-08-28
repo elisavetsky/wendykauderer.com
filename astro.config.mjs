ff2600be38f9d373ca489fca988ed63907f2f5e6
gabesean <code.approved@slmail.me>
2023-08-28 14:29:05 -0700

    initial commit...setting the astro launchpad

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  integrations: [mdx(), tailwind()]
+});
