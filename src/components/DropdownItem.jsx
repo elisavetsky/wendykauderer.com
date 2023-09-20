65f67c82804baa4c61e2919760ad98166d110aa8
gabesean <code.approved@slmail.me>
2023-09-19 22:43:48 -0700

    convert DropdownItem.astro to React component

---
+++ src/components/DropdownItem.jsx
@@ -0,0 +1,7 @@
+export default function DropdownItem({item}) {
+   return (
+      <li>
+         <a class="block w-full px-4 transition-[inherit] hover:bg-slate-300/50 hover:text-slate-800 dark:hover:text-slate-100 dark:hover:bg-slate-700/50" href={item.href}>{item.title}</a>
+      </li>
+   )
+}
