d31e59bc9e581b1493f9e17b72dbbbcce5db46cf
gabesean <code.approved@slmail.me>
2023-09-20 12:18:08 -0700

    tweak page design, move ArtTypes.astro to Header

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,9 +1,17 @@
+import Link from "./Link.jsx"
+
 export default function Tag({title, artType}) {
-   const commonClasses = "transition-colors duration-700 h-fit px-2 py-0.5 last-of-type:mr-0 flex items-center rounded-full text-slate-700 dark:text-slate-300"
+   const commonClasses = "transition-colors h-fit px-2 py-0.5 last-of-type:mr-0 flex items-center rounded-full text-slate-700 dark:text-slate-300"
 
    return (
       artType ? 
-      <span class={`${commonClasses} bg-cyan-50/70 dark:bg-cyan-950`}>{artType}</span>
+      <Link 
+         inline 
+         href={`/${artType}`}
+         classes="no-underline"
+      >
+         <span class={`${commonClasses} bg-cyan-50/70 hover:bg-cyan-100/70 dark:bg-cyan-950 dark:hover:bg-cyan-800`}>{artType}</span>
+      </Link>
       : 
       <span class={`${commonClasses} bg-slate-100 dark:bg-slate-800`}>{title}</span>
    )
