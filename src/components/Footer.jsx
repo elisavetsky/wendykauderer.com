8b907f21ded8bbdb3759e7ce32144fb6fdef0a52
gabesean <code.approved@slmail.me>
2023-10-01 12:23:28 -0700

    tweak styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,18 @@ import Link from "./Link.jsx";
 
 export default function Footer({footerNav}) {
    return (
-      <footer className="mt-auto w-screen border-t-slate-400 border-t dark:border-t-slate-500 sm:w-full">
-         <div className="h-full px-4 pt-48 pb-7 flex flex-col gap-20 mx-auto max-w-screen-2xl justify-between sm:text-right md:items-center lg:flex-row lg:items-end">
+      <footer className="mt-auto sm:w-full">
+         <div className="h-full px-4 pt-48 pb-7 flex flex-col gap-20 mx-auto max-w-screen-2xl justify-between sm:text-right md:items-center md:px-0 lg:px-4 lg:flex-row lg:items-end">
             <div className="flex-auto text-left self-start lg:self-end">
                <Link
                   href="/"
                   inline
                   transition="transition"
-                  classes="text-xl motion-safe:hover:-translate-y-[2px] motion-safe:hover:translate-x-[2px] hover:text-slate-400 dark:hover:text-slate-400"
+                  classes="font-semibold motion-safe:hover:-translate-y-[2px] motion-safe:hover:translate-x-[2px] hover:text-slate-400 dark:hover:text-zinc-400"
                >
                   {SitewideConfig.frontmatter.site_name}
                </Link>
-               <ul className="text-md mt-4">
+               <ul className="text-md mt-4 border-b-gray-300 border-b dark:border-b-zinc-600">
                   {footerNav.map(({title, href, dropdown_items}) => {
                      if (dropdown_items) {
                         return dropdown_items.map(({title, href}) => {
@@ -43,7 +43,7 @@ export default function Footer({footerNav}) {
                         return (
                            <li
                               key={title} 
-                              className="mb-2 last-of-type:mb-0"
+                              className="mb-2 last-of-type:mb-6"
                            >
                               <Link
                                  href={href}
@@ -59,7 +59,7 @@ export default function Footer({footerNav}) {
                   })}
                </ul>
             </div>
-            <div className="text-sm flex flex-wrap sm:h-fit sm:flex-wrap-nowrap sm:flex-row sm:self-start sm:items-center lg:justify-end lg:self-end">
+            <div className="text-md flex flex-wrap sm:h-fit sm:flex-wrap-nowrap sm:flex-row sm:self-start sm:items-center lg:justify-end lg:self-end">
                <span className="transition-colors mr-1">A website by</span>
                   <Link
                      href={SitewideConfig.frontmatter.developer_site_url}
--- src/components/ImageCTA.jsx
+++ src/components/ImageCTA.jsx
@@ -1,8 +1,8 @@
 export default function ImageCTA({children, image}) {
    return (
-      <div className="transition-colors duration-700 max-w-xl w-fit font-medium text-2xl mt-2 px-5 leading-tight md:px-0 md:pr-5 lg:font-light lg:text-3xl">
-         <div className="flex flex-col drop-shadow-lg gap-4 md:grid md:grid-flow-col md:auto-cols-auto lg:min-w-[13rem] lg:max-w-[14rem] lg:grid-flow-row">{children}</div>
-         <span className="flex mt-5 lg:mt-9 border-b-slate-400 border-b"></span>
+      <div className="transition-colors duration-700 max-w-xl w-fit font-medium text-2xl mt-2 px-5 md:px-0 md:pr-5 lg:font-medium lg:text-3xl">
+         <div className="flex flex-col gap-4 md:grid md:grid-flow-col md:auto-cols-auto lg:min-w-[13rem] lg:max-w-[14rem] lg:grid-flow-row lg:gap-6">{children}</div>
+         <span className="flex mt-5 lg:mt-10 border-b-slate-800 border-b dark:border-b-zinc-100"></span>
       </div>
    )
 }
