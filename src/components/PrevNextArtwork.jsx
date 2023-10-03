1b3d684d66c49bbb3ba408767a5d0f999e02d7f1
gabesean <code.approved@slmail.me>
2023-10-02 23:08:43 -0700

    tweak a little PrevNextArtwork.jsx styles

--- src/components/PrevNextArtwork.jsx
+++ src/components/PrevNextArtwork.jsx
@@ -1,20 +1,24 @@
 export default function PrevNextArtwork({prevEntry, nextEntry}) {
 
    // Do not render this if there is nothing previous or next
-   if (prevEntry !== null && nextEntry !== null ) {
+   if (prevEntry ?? nextEntry) {
       return (
          <nav className="flex justify-between py-4 gap-4" aria-label="previous artwork and next artwork navigation">
             {prevEntry &&
-               <a href={`/${prevEntry.data.art_type}/${prevEntry.slug}`} className="items-start flex flex-auto flex-col">
-                  <span>˿</span>
-                  <span>{prevEntry.data.title}</span>
-               </a>
+               <div className="mr-auto">
+                  <a href={`/${prevEntry.data.art_type}/${prevEntry.slug}`} className="w-fit items-start flex flex-col">
+                     <span>˿</span>
+                     <span>{prevEntry.data.title}</span>
+                  </a>
+               </div>
             }
             {nextEntry &&
-               <a href={`/${nextEntry.data.art_type}/${nextEntry.slug}`} className="items-end flex flex-auto flex-col">
-                  <span>˲</span>
-                  <span>{nextEntry.data.title}</span>
-               </a>
+               <div className="ml-auto">
+                  <a href={`/${nextEntry.data.art_type}/${nextEntry.slug}`} className="w-fit items-end flex flex-col">
+                     <span>˲</span>
+                     <span>{nextEntry.data.title}</span>
+                  </a>
+               </div>
             }
          </nav>
       )
