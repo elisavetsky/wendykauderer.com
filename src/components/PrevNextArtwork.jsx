6309efb0dfcad9e4fcb5489ca2dc9a9c12ec12c4
gabesean <code.approved@slmail.me>
2023-10-04 15:35:40 -0700

    begin attempt to fix position of PrevNextArtwork for better UX

--- src/components/PrevNextArtwork.jsx
+++ src/components/PrevNextArtwork.jsx
@@ -8,15 +8,15 @@ export default function PrevNextArtwork({prevEntry, nextEntry}) {
       return (
          <nav 
             aria-label={`previous ${prevEntry?.data.art_type ?? nextEntry?.data.art_type} and next artwork navigation`}
-            className="text-sm grid grid-cols-2 ml-auto justify-between py-4 gap-3 xl:text-base"
+            className="fixed bottom-0 left-0 px-4 w-full z-30 text-sm grid grid-cols-2 ml-auto justify-between py-4 gap-3 sm:static sm:px-0 xl:text-base"
          >
             {prevEntry &&
                <a 
                   href={`/${prevEntry.data.art_type}/${prevEntry.slug}`} 
                   aria-label={`Previous ${prevEntry.data.art_type}`}
-                  className="group transition-colors border-2 items-start flex flex-col justify-between gap-4 px-3 pb-2 pt-3 border-black/10 hover:bg-black/10 dark:border-white/10"
+                  className="group bg-white/50 backdrop-blur-lg transition-colors border-2 items-end flex justify-between gap-4 px-3 pb-2 pt-3 border-black/10 hover:bg-black/10 dark:bg-black/30 dark:border-white/10 sm:flex-col sm:items-start"
                >
-                  <ArrowLeftIcon className='w-8 transition motion-safe:group-hover:-translate-x-1' aria-hidden />
+                  <ArrowLeftIcon className='w-5 sm:w-8 transition motion-safe:group-hover:-translate-x-1' aria-hidden />
                   <span className="">{prevEntry.data.title}</span>
 
                </a>
@@ -25,9 +25,9 @@ export default function PrevNextArtwork({prevEntry, nextEntry}) {
                <a 
                   href={`/${nextEntry.data.art_type}/${nextEntry.slug}`} 
                   aria-label={`Next ${nextEntry.data.art_type}`}
-                  className="group text-right transition-colors col-start-2 border-2 items-end flex flex-col justify-between gap-4 px-3 pb-2 pt-3 border-black/10 hover:bg-black/10 dark:border-white/10"
+                  className="group bg-white/50 backdrop-blur-lg text-left transition-colors col-start-2 border-2 items-end flex flex-row-reverse justify-between gap-4 px-3 pb-2 pt-3 border-black/10 hover:bg-black/10 dark:bg-black/30 dark:border-white/10 sm:flex-col sm:text-right"
                >
-                  <ArrowRightIcon className='w-8 transition motion-safe:group-hover:translate-x-1' aria-hidden />
+                  <ArrowRightIcon className='w-5 sm:w-8 transition motion-safe:group-hover:translate-x-1' aria-hidden />
                   <span className="self-end">{nextEntry.data.title}</span>
                </a>
             }
