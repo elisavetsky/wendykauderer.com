066c39ee34692b44d657b8280ded280947473b5d
gabesean <code.approved@slmail.me>
2023-10-04 15:04:31 -0700

    tweak shadows & hover shadows on FullArtwork and SoldMarker

--- src/components/SoldMarker.jsx
+++ src/components/SoldMarker.jsx
@@ -4,7 +4,7 @@ export default function SoldMarker({ sold }) {
       return (
          <span 
             aria-label="This artwork is unavailable" 
-            class="w-4 h-4 rounded-full rotate-45 absolute bottom-2 left-2 border shadow-md shadow-black border-black/40 backdrop-blur-md bg-white/80 dark:bg-black/80 dark:border-white/40"
+            class="w-4 h-4 rounded-full rotate-45 absolute bottom-2 left-2 border shadow-md shadow-black/70 border-black/40 backdrop-blur-md bg-white/80 dark:bg-black/80 dark:border-white/40"
          >
          </span>
       )
