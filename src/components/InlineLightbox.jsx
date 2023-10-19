26876ca3ebf7d9ada705ee5bc631dfe040d29d6c
gabesean <code.approved@slmail.me>
2023-10-19 01:14:12 -0700

    add responsive images to InlineLightbox
    Figured out the origin of the breaking css. IN SHORT: DON'T IMPORT ASTRO IMAGE IN JSX

--- src/components/InlineLightbox.jsx
+++ src/components/InlineLightbox.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-// Astro
+/////////////////////////////////////////////////////
+// DO NOT REMOVE THIS LINE BELOW                  ///
+// OR THE STYLES WILL COMPLETELY BREAK!!!!        ///
+// POSSIBLE SOLUTION: 								     ///
+// MOVE EVERYTHING EXCEPT THIS LINE TO A NEW FILE ///
+/////////////////////////////////////////////////////
 import { Image } from "astro:assets";
+
+
+// import utils
+import { getImageSrcSet, getAspectRatio } from "../utils/imageTools";
 
 // Import lightbox
 import Lightbox from "yet-another-react-lightbox";
 import Inline from "yet-another-react-lightbox/plugins/inline";
+import Counter from "yet-another-react-lightbox/plugins/counter";
+
+// import lightbox styles
 import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/counter.css";
 
 // import custom icons
 import { 
@@ -14,36 +27,85 @@ import {
 	ChevronRightIcon, 
 } from '@heroicons/react/24/outline';
 
-export default function InlineLightbox({imageArr}) {
-	const [open, setOpen] = useState(false);
+export default function InlineLightbox({images, classes, children}) {
+	const [isLoaded, setIsLoaded] = useState(false);
 
-	const slides = imageArr.map(({image, image_alt}) => {
+	useEffect(() => {
+		setIsLoaded(true)
+	}, [])
+
+	const slides = images.optimizedImages?.map(({src, attributes, srcSet}, i) => {
 		return {
-			src: image.src,
-			alt: image_alt,
-			width: image.width,
-			height: image.height
+			src: src,
+ 			alt: images.imagesWithAlts[i].image_alt, // get alt by matching index from optimizedImages
+			width: attributes.width,
+			height: attributes.height,
+ 			srcSet: getImageSrcSet(attributes.width, attributes.height, srcSet.values) // get srcSet for each additional image with helper function
 		}
 	})
 
+	const imageAspectRatio = getAspectRatio(slides[0].width, slides[0].height);
+
 	return (
-		<Lightbox
-			plugins={[Inline]}
-			slides={slides}
-			carousel={{
-				imageFit: "cover",
-				spacing: 10
-			}}
-			render={{
-				iconPrev: () => <ChevronLeftIcon className="w-8 h-8" />,
-				iconNext: () => <ChevronRightIcon className="w-8 h-8" />,
-			}}
-			className={"shadow-xl"}
-			styles={{
-				container: { borderRadius: "0.5rem", position: "relative" },
-				slide: { padding: "0", maxHeight: "100vh" }
-				// slide: { padding: "0", height: "auto", maxWidth: "768px", width: "100%" },
-			}}
-		/>
-	);
+		<div className={`${classes} relative` || null}>
+			<noscript className="absolute z-10">
+				<ul className=" grid grid-cols-2 gap-1">
+					{images.optimizedImages?.map(({src, attributes}, i) => {
+						return (
+							<li key={src}>
+								<img 
+									src={src} 
+									alt={images.imagesWithAlts[i].image_alt} 
+									width={attributes.width}
+									height={attributes.height}
+									class="rounded-lg"
+								/>
+							</li>
+						)
+					})}
+				</ul>
+			</noscript>
+			<Lightbox
+				plugins={[Inline, Counter]}
+				counter={{ 
+					container: { 
+						style: { 
+							fontSize: "14px",
+							fontWeight: 600,
+							marginLeft: "0.75rem" 
+						} 
+					} 
+				}}
+				slides={slides}
+				carousel={{
+					imageFit: "cover",
+					spacing: 10
+				}}
+				render={{
+					iconPrev: () => <ChevronLeftIcon className="w-8 h-8" />,
+					iconNext: () => <ChevronRightIcon className="w-8 h-8" />,
+					buttonPrev: slides.length <= 1 ? () => null : undefined,
+					buttonNext: slides.length <= 1 ? () => null : undefined,
+				}}
+				animation={{
+					fade: 300,
+					swipe: 900,
+					navigation: 900,
+					easing: {
+						fade: "ease-in-out",
+						swipe: "cubic-bezier(0.18, 0.89, 0.44, 1)",
+						navigation: "cubic-bezier(0.18, 0.89, 0.44, 1)",
+					},
+				}}
+				className={"shadow-xl"}
+				styles={{
+					root: { 
+						visibility: isLoaded ? "visible" : "hidden",
+					},
+					container: { borderRadius: "0.5rem", position: "relative", aspectRatio: imageAspectRatio },
+					slide: { padding: "0", maxHeight: "100vh" }
+				}}
+			/>
+		</div>
+	)
 }
