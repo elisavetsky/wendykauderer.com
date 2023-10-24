50876f5d944b51df70d33b5a4bf799b4b1568cb5
gabesean <code.approved@slmail.me>
2023-10-24 00:28:58 -0700

    tweak MainFlexLayout padding

--- src/components/layouts/MainFlexLayout.jsx
+++ src/components/layouts/MainFlexLayout.jsx
@@ -12,7 +12,7 @@ export default function MainFlexLayout({ header, children }) {
       <div className="flex flex-col md:flex-row  mx-auto grow items-start min-w-full max-w-screen-2xl gap-4">
          {header}
          {/* <Header /> */}
-         <div className="relative w-full text-primary-text dark:text-primary-dark-text m-auto flex flex-col min-h-[125vh] grow md:pr-8 md:w-9/12 lg:m-0">
+         <div className="relative w-full text-primary-text dark:text-primary-dark-text m-auto flex flex-col min-h-[125vh] grow md:pr-5 md:w-9/12 lg:m-0">
             <main id="main-content" className="max-[233px]:mt-10 pb-16 pt-20 md:pt-20">
                {children}
                
