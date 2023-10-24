89d4333693bff508cfd85d621dd68eb2fed672a8
gabesean <code.approved@slmail.me>
2023-10-24 14:38:31 -0700

    refactor some components

---
+++ src/components/ImageColumn.jsx
@@ -0,0 +1,8 @@
+export default function ImageColumn({children}) {
+
+   return (
+      <div className="p-4 m-auto md:m-0 lg:w-auto xl:m-auto xl:w-3/5 2xl:w-3/4">
+         {children}
+      </div>
+   )
+}
