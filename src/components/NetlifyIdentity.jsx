a255e61158b9ffc54b0283c39cce3485174c0f45
gabesean <code.approved@slmail.me>
2023-11-07 11:44:33 -0800

    fix: redirect to admin after login

--- src/components/NetlifyIdentity.jsx
+++ src/components/NetlifyIdentity.jsx
@@ -16,6 +16,7 @@ function NetlifyIdentity() {
    if (window.netlifyIdentity) {
 
       window.netlifyIdentity.on("init", (user) => {
+
          if (!user) {
             window.netlifyIdentity.on("login", () => {
                document.location.href = "/admin/";
@@ -26,6 +27,14 @@ function NetlifyIdentity() {
             });
          }
       });
+
+      window.netlifyIdentity.on("login", () => {
+         document.location.href = "/admin/";
+      })
+
+      window.netlifyIdentity.on("logout", () => {
+         document.location.href = "/admin/";
+      })
    }
 }
 
