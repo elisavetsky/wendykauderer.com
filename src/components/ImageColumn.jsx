96fdee585aa2b90c53fdf9cc5a34ddd4a21aa8f4
gabesean <code.approved@slmail.me>
2023-11-07 15:11:05 -0800

    feat: improve `Flex2ColLayout.jsx` & tweak related structures

--- src/components/ImageColumn.jsx
+++ src/components/ImageColumn.jsx
@@ -1,7 +1,7 @@
 export default function ImageColumn({children}) {
 
    return (
-      <div className="p-4 m-auto md:m-0 lg:w-auto xl:m-auto xl:w-3/5 2xl:w-3/4">
+      <div className="p-4 m-auto md:m-0 lg:w-auto ">
          {children}
       </div>
    )
